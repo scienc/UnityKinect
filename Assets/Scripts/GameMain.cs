43a2ec8f082521e0629c037fcf2808a7ded666ae
stf <songtiefei@edroity.com>
2019-07-18 15:27:33 +0800

    Init kinect

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+    private void Awake () {
+        for (int i = 0; i < Display.displays.Length; i++) {
+            Display.displays[i].Activate ();
+            Screen.SetResolution (Screen.width, Screen.height, true);
+        }
+        Debug.Log ("Display.displays " + Display.displays.Length);
+    }
+
+    private void Update () {
+        if (Input.GetKeyDown (KeyCode.Escape)) {
+            Application.Quit ();
+        }
+    }
+}
